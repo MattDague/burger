86978a581e01bc13abf3ba163aa536e5bc7f2427
Matthew Dague <mattdague@gmail.com>
2019-08-06 17:33:17 -0400

    added pw and connection to heroku

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (burger_name) VALUES ('Baconator');
+INSERT INTO burgers (burger_name) VALUES ('Big Mac');
+INSERT INTO burgers (burger_name) VALUES ('Royale with Cheese');
+INSERT INTO burgers (burger_name, devoured) VALUES ('Krabby Patty', true);
