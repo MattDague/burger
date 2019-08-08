c0114a2fce94a0ee7c50c6afadcab6d207454e60
Matthew Dague <mattdague@gmail.com>
2019-08-08 10:04:26 -0400

    linked js and removed it from handlebars

--- public/assets/js/burgerJava.js
+++ public/assets/js/burgerJava.js
@@ -1,43 +1,37 @@
-
 $(function() {
-    $(".uneaten").on("click", function(event) {
-      var id = $(this).data("id");
-      var devour = $(this).data("devour");
-  
-      var eatStatus = {
-        devoured: true
-      };
-      $.ajax("/api/burgers/" + id, {
-        type: "PUT",
-        data: eatStatus
-      }).then(
-        function() {
-          console.log("ate burger", devour);
-          location.reload();
-        }
-      );
-    });
-  
-    // $(".create-form").on("submit", function(event) {
-    //   // Make sure to preventDefault on a submit event.
-    //   event.preventDefault();
-  
-    //   var newCat = {
-    //     name: $("#ca").val().trim(),
-    //     sleepy: $("[name=sleepy]:checked").val().trim()
-    //   };
-  
-    //   // Send the POST request.
-    //   $.ajax("/api/cats", {
-    //     type: "POST",
-    //     data: newCat
-    //   }).then(
-    //     function() {
-    //       console.log("created new cat");
-    //       // Reload the page to get the updated list
-    //       location.reload();
-    //     }
-    //   );
-    // });
+  $(".uneaten").on("click", function(event) {
+    var id = $(this).data("id");
+    var devour = $(this).data("devour");
+
+    var eatStatus = {
+      devoured: true
+    };
+    $.ajax("/api/burgers/" + id, {
+      type: "PUT",
+      data: eatStatus
+    }).then(
+      function() {
+        console.log("ate burger", devour);
+        location.reload();
+      }
+    );
   });
-  
+
+  $(".add-burger").on("submit", function(event){
+      console.log("new burger added");
+      event.preventDefault();
+      var newBurger = {
+          burger_name: $("#burgerAdd").val().trim(),
+      }
+      console.log("new burger added");
+      $.ajax("/api/burgers", {
+          type: "POST",
+          data: newBurger
+      }).then(
+          function(){
+              console.log("new burger added");
+              location.reload();
+          }
+      )
+  });
+});
